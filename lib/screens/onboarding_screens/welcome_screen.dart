e41bd6bc2613b4a828507a3d238042741b01cfaa
suman2023 <sumanmitra07@outlook.com>
2022-03-22 21:00:42 +0530

    circular bar while loading,and route to Login Screen

--- lib/screens/onboarding_screens/welcome_screen.dart
+++ lib/screens/onboarding_screens/welcome_screen.dart
@@ -22,7 +22,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4');
     _videoPlayerController.setLooping(false);
   }
-Future<VideoPlayerController> getController() async {
+
+  Future<VideoPlayerController> getController() async {
     await _videoPlayerController.initialize();
     await _videoPlayerController.setLooping(false);
     return _videoPlayerController;
@@ -40,82 +41,83 @@ Future<VideoPlayerController> getController() async {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return SafeArea(
-      Scaffold(
-      body: Stack(
-        fit: StackFit.loose,
-        children: [
-          FutureBuilder(
-              future: getController(),
-              builder: (context,
-                  AsyncSnapshot<VideoPlayerController> playerSnapshot) {
-                if (playerSnapshot.hasData) {
-                  playerSnapshot.data!.play();
+      child: Scaffold(
+        body: Stack(
+          fit: StackFit.loose,
+          children: [
+            FutureBuilder(
+                future: getController(),
+                builder: (context,
+                    AsyncSnapshot<VideoPlayerController> playerSnapshot) {
+                  if (playerSnapshot.hasData) {
+                    playerSnapshot.data!.play();
 
-                  return playerSnapshot.connectionState ==
-                          ConnectionState.waiting
-                      ? Center(child: CircularProgressIndicator())
-                      : playerSnapshot.connectionState == ConnectionState.done
-                          ? SizedBox.expand(
-                              child: FittedBox(
-                                fit: BoxFit.fill,
-                                child: SizedBox(
-                                  width:
-                                      _videoPlayerController.value.size.width,
-                                  height:
-                                      _videoPlayerController.value.size.height,
-                                  child: VideoPlayer(_videoPlayerController),
+                    return playerSnapshot.connectionState ==
+                            ConnectionState.waiting
+                        ? Center(child: CircularProgressIndicator())
+                        : playerSnapshot.connectionState == ConnectionState.done
+                            ? SizedBox.expand(
+                                child: FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: SizedBox(
+                                    width:
+                                        _videoPlayerController.value.size.width,
+                                    height: _videoPlayerController
+                                        .value.size.height,
+                                    child: VideoPlayer(_videoPlayerController),
+                                  ),
                                 ),
-                              ),
-                            )
-                          : Center(
-                              child: CircularProgressIndicator(
-                                semanticsLabel: "Loading",
-                              ),
-                            );
-                } else if (playerSnapshot.hasError) {
-                  return Center(
-                    child: IconButton(
-                        onPressed: () {
-                          setState(() {});
-                        },
-                        icon: Icon(Icons.star)),
-                  );
-                } else {
-                  return Center(
-                      child: CircularProgressIndicator(
-                    semanticsLabel: "Loading",
-                  ));
-                }
-              }),
-          Align(
-            alignment: Alignment.topRight,
-            child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (_) => LoginSignupScreen()));
-                },
-                child: Text("skip >>")),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all(EdgeInsets.symmetric(
-                        horizontal: width * .3, vertical: 18.0)),
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8))),
-                  ),
+                              )
+                            : Center(
+                                child: CircularProgressIndicator(
+                                  semanticsLabel: "Loading",
+                                ),
+                              );
+                  } else if (playerSnapshot.hasError) {
+                    return Center(
+                      child: IconButton(
+                          onPressed: () {
+                            setState(() {});
+                          },
+                          icon: Icon(Icons.star)),
+                    );
+                  } else {
+                    return Center(
+                        child: CircularProgressIndicator(
+                      semanticsLabel: "Loading",
+                    ));
+                  }
+                }),
+            Align(
+              alignment: Alignment.topRight,
+              child: TextButton(
                   onPressed: () {
                     Navigator.of(context).pushReplacement(
                         MaterialPageRoute(builder: (_) => LoginSignupScreen()));
                   },
-                  child: Text("Next")),
+                  child: Text("skip >>")),
             ),
-          )
-        ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all(EdgeInsets.symmetric(
+                          horizontal: width * .3, vertical: 18.0)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (_) => LoginSignupScreen()));
+                    },
+                    child: Text("Next")),
+              ),
+            )
+          ],
+        ),
       ),
-    ),);
+    );
   }
 }
