d7a6cd078f9b69e1693dfd634d289dd1291bb367
suman2023 <sumanmitra07@outlook.com>
2022-03-25 17:48:29 +0530

    if just loged in u select or go to home, else have access to my habits

--- lib/screens/onboarding_screens/select_habits.dart
+++ lib/screens/onboarding_screens/select_habits.dart
@@ -2,10 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:syalo/screens/habits_screen/define_habits.dart';
+import 'package:syalo/screens/habits_screen/ongoing_habits.dart';
 import 'package:syalo/screens/mainframe.dart';
 
 class SelectHabits extends StatefulWidget {
-  const SelectHabits({Key? key}) : super(key: key);
+  bool? firstPage;
+  SelectHabits({Key? key, this.firstPage}) : super(key: key);
 
   @override
   _SelectHabitsState createState() => _SelectHabitsState();
@@ -21,17 +23,17 @@ class _SelectHabitsState extends State<SelectHabits> {
     {
       'name': "Yoga1",
       'imageURL':
-          "https://media.istockphoto.com/photos/my-presence-is-my-power-picture-id1303002202?s=612x612"
+          "https://cdn.pixabay.com/photo/2014/12/16/22/25/sunset-570881_960_720.jpg",
     },
     {
       'name': "Yoga2",
       'imageURL':
-          "https://media.istockphoto.com/photos/my-presence-is-my-power-picture-id1303002202?s=612x612"
+          "https://cdn.pixabay.com/photo/2016/11/29/09/49/woman-1868817_960_720.jpg"
     },
     {
       'name': "Yoga3",
       'imageURL':
-          "https://media.istockphoto.com/photos/my-presence-is-my-power-picture-id1303002202?s=612x612"
+          "https://media.istockphoto.com/photos/my-presence-is-my-power-picture-id1303002202?s=612x612",
     },
     {
       'name': "Yoga4",
@@ -41,7 +43,7 @@ class _SelectHabitsState extends State<SelectHabits> {
     {
       'name': "Yoga5",
       'imageURL':
-          "https://media.istockphoto.com/photos/my-presence-is-my-power-picture-id1303002202?s=612x612"
+          "https://cdn.pixabay.com/photo/2013/05/11/08/28/sunset-110305_960_720.jpg",
     },
     {
       'name': "Yoga6",
@@ -51,7 +53,7 @@ class _SelectHabitsState extends State<SelectHabits> {
     {
       'name': "Yoga7",
       'imageURL':
-          "https://media.istockphoto.com/photos/my-presence-is-my-power-picture-id1303002202?s=612x612"
+          "https://cdn.pixabay.com/photo/2015/01/07/15/51/woman-591576_960_720.jpg",
     },
   ];
 
@@ -60,90 +62,75 @@ class _SelectHabitsState extends State<SelectHabits> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Text(
+            "Add Habits",
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          actions: widget.firstPage == true
+              ? []
+              : [
+                  TextButton(
+                      onPressed: () => Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => OngoingHabits())),
+                      child: Text("My Habits"))
+                ],
+        ),
         body: Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 70,
-            child: Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    print(
-                        "if this is for first time it is supposed to work the same way");
-                    print(
-                        "But if already account is set up this should go to home");
-                    print(
-                        "Unimplemented because local storage is not set up for the moment");
-                    print(
-                        "Same goes for the below button where adding 3 habits is mandatory for first time");
-                    Navigator.pop(context);
-                  },
-                  child: Icon(
-                    Icons.chevron_left,
-                    size: 40,
-                  ),
+          alignment: AlignmentDirectional.center,
+          children: [
+            Positioned(
+              top: 0,
+              child: Container(
+                height: height,
+                width: width,
+                child: GridView.count(
+                  // shrinkWrap: true,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                  children: [
+                    for (int i = 0; i < habits.length; i++)
+                      gridTile(height, width, habits[i]),
+                    Container()
+                  ],
                 ),
-                SizedBox(
-                  width: 20,
-                ),
-                Text(
-                  "Add Habits",
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-        Positioned(
-          top: 70,
-          child: Container(
-            height: height,
-            width: width,
-            child: GridView.count(
-              // shrinkWrap: true,
-              crossAxisCount: 2,
-              crossAxisSpacing: 12,
-              mainAxisSpacing: 12,
-              children: [
-                for (int i = 0; i < habits.length; i++)
-                  gridTile(height, width, habits[i]),
-                Container()
-              ],
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all(EdgeInsets.symmetric(
-                      horizontal: width * .3, vertical: 18.0)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8))),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => MainFrame()));
-//                   Navigator.push(
-//                       context,
-//                       MaterialPageRoute(
-//                           builder: (_) => HabitsDescription(habitName: "")));
-                  // TODO: Navigate to home page after atleast 3 selection
-//                 },
-//                 child: Text("Create My Own")),
-                },
-                child: Text("Go to Home"),
-              )),
-        )
-      ],
-    ));
+            Positioned(
+              bottom: 50,
+              child: Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all(EdgeInsets.symmetric(
+                          horizontal: width * .3, vertical: 18.0)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                    ),
+                    onPressed: () {
+                      widget.firstPage == true
+                          ? Navigator.push(context,
+                              MaterialPageRoute(builder: (_) => MainFrame()))
+                          : Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) =>
+                                      HabitsDescription(habitName: "")));
+                      // TODO: Navigate to home page after atleast 3 selection
+                      //                 },
+                      //                 child: Text("Create My Own")),
+                    },
+                    child: widget.firstPage == true
+                        ? Text("Go to Home")
+                        : Text("Create my own"),
+                  )),
+            )
+          ],
+        ));
   }
 
   Widget gridTile(double height, double width, Map<String, String> habit) {
