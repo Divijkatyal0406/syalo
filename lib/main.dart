1529cbe3927fde94c91d82c99ce952f099a85e30
suman2023 <sumanmitra07@outlook.com>
2022-03-25 17:43:36 +0530

    if not logged in start from onboarding else mainframe

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mixpanel_flutter/mixpanel_flutter.dart';
+import 'package:syalo/screens/habits_screen/ongoing_habits.dart';
 import 'package:syalo/screens/login_signup.dart';
 import 'package:syalo/habbits/habbitHome.dart';
 import 'package:syalo/screens/home_screen.dart';
 import 'package:syalo/screens/mainframe.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:syalo/screens/onboarding_screens/select_habits.dart';
+import 'package:syalo/screens/onboarding_screens/welcome_screen.dart';
 import 'package:syalo/screens/onboarding_video.dart';
 import 'package:syalo/songTracks/homePage.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,7 +27,6 @@ class SyaloApp extends StatefulWidget {
 }
 
 class _SyaloAppState extends State<SyaloApp> {
-
   late Mixpanel mixpanel;
 
   @override
@@ -33,20 +34,24 @@ class _SyaloAppState extends State<SyaloApp> {
     super.initState();
     initMixpanel();
   }
+
   Future<void> initMixpanel() async {
-    mixpanel = await Mixpanel.init("1e307809e3aca420e9241fc84d0164cf", optOutTrackingDefault: false);
+    mixpanel = await Mixpanel.init("1e307809e3aca420e9241fc84d0164cf",
+        optOutTrackingDefault: false);
   }
 
   @override
   Widget build(BuildContext context) {
-    //FirebaseAuth.instance.signOut();
+    // FirebaseAuth.instance.signOut();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SuprSelf',
       // theme: ThemeData(
       // ),
-      // home: FirebaseAuth.instance.currentUser!=null ? const MainFrame(): LoginSignupScreen(),
-      home: MainFrame(),
+      home: FirebaseAuth.instance.currentUser != null
+          ? const MainFrame()
+          : const WelcomeScreen(),
+      // home: ,
     );
   }
 }
