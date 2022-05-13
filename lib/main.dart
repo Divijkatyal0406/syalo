d65500efbf044238fb504cda220b5e1ced80f9e7
suman2023 <sumanmitra07@outlook.com>
2022-05-13 21:14:34 +0530

    small modifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mixpanel_flutter/mixpanel_flutter.dart';
-import 'package:syalo/database/db.dart';
-import 'package:syalo/journals/journalHome.dart';
-import 'package:syalo/screens/login_signup.dart';
-import 'package:syalo/habbits/habbitHome.dart';
-import 'package:syalo/screens/home_screen.dart';
 import 'package:syalo/screens/mainframe.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:syalo/screens/onboarding_screens/welcome_screen.dart';
-import 'package:syalo/screens/onboarding_video.dart';
-import 'package:syalo/screens/profile_screen.dart';
-import 'package:syalo/songTracks/homePage.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,7 +19,6 @@ class SyaloApp extends StatefulWidget {
 }
 
 class _SyaloAppState extends State<SyaloApp> {
-
   late Mixpanel mixpanel;
 
   @override
@@ -37,8 +26,10 @@ class _SyaloAppState extends State<SyaloApp> {
     super.initState();
     initMixpanel();
   }
+
   Future<void> initMixpanel() async {
-    mixpanel = await Mixpanel.init("1e307809e3aca420e9241fc84d0164cf", optOutTrackingDefault: false);
+    mixpanel = await Mixpanel.init("1e307809e3aca420e9241fc84d0164cf",
+        optOutTrackingDefault: false);
   }
 
   @override
@@ -46,13 +37,15 @@ class _SyaloAppState extends State<SyaloApp> {
     // FirebaseAuth.instance.signOut();
     //FireStoreDB().addHabit("Random Happy Faces",1);
     //print(FireStoreDB().getHabitsList());
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SuprSelf',
       // theme: ThemeData(
       // ),
-      home: FirebaseAuth.instance.currentUser!=null ? const MainFrame(): const WelcomeScreen(),
-      // home: HabbitScreen(),
+      // home: FirebaseAuth.instance.currentUser != null
+      //     ? const MainFrame()
+      //     : const WelcomeScreen(),
+      home: WelcomeScreen(),
     );
   }
 }
